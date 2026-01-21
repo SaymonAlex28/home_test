1bc4c50bd178094876450f04d1cc92b77ed6e24b
SaymonAlex28 <145854670+SaymonAlex28@users.noreply.github.com>
2026-01-21 22:17:15 +0200

    commit 8

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'esp-ui-v1';
+const CACHE_NAME = 'esp-ui-v3';
 
 const FILES_TO_CACHE = [
   './',
@@ -29,5 +29,4 @@ self.addEventListener('activate', e => {
     )
   );
   self.clients.claim();
-
 });
